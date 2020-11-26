d05f384d10d1c7e89e5491c21e8c185ccbb0cb1f
betobr12 <betobr12@yahoo.com.br>
2020-11-26 16:29:00 -0300

    firt commit

---
+++ app/Http/Controllers/ControladorProduto.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Categoria;
+use App\Produto;
+use Illuminate\Http\Request;
+
+class ControladorProduto extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function indexView()
+    {
+        $produtos = Produto::all(); 
+        return view('produtos', compact('produtos'));
+    }
+
+    public function index()
+    {
+        $produtos = Produto::all(); 
+        return json_encode($produtos);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+        $categorias = Categoria::all();
+
+        return view('novoproduto',compact('categorias'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $prod = new Produto();
+        $prod->nome = $request->input('nome');
+        $prod->preco = $request->input('preco');
+        $prod->estoque = $request->input('estoque');
+        $prod->categoria_id = $request->input('categoria_id');
+        $prod->save();
+        return json_encode($prod);
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)        
+    {
+        $categorias = Categoria::all();
+        $produto = Produto::find($id);
+        if(isset($produto)) {
+            return view('editarproduto', compact('produto','categorias'));
+        }
+        return redirect('/produtos');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $produto = Produto::find($id);
+        if(isset($produto)) {
+            $produto->nome          = $request->input('nomeProduto');
+            $produto->estoque       = $request->input('estoqueProduto');
+            $produto->preco         = $request->input('precoProduto');
+            $produto->categoria_id  = $request->input('categoria_id');
+            $produto->save();
+        }
+        return redirect('/produtos');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $produto = Produto::find($id);
+        if (isset($produto)) {
+            $produto->delete();
+        }
+        return redirect('/produtos');
+    }
+
+}
