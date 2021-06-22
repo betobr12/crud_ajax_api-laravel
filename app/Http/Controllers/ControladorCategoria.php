7ceb3057e62f5035b0d8c484e30dfc29ce57fb59
Roberto <betobr12@users.noreply.github.com>
2021-06-22 13:38:12 -0300

    Update ControladorCategoria.php

--- app/Http/Controllers/ControladorCategoria.php
+++ app/Http/Controllers/ControladorCategoria.php
@@ -8,33 +8,18 @@ use App\Categoria;
 
 class ControladorCategoria extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $cats = Categoria::all();
         return view('categorias', compact('cats'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('novacategoria');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $cat = new Categoria();
@@ -43,23 +28,6 @@ class ControladorCategoria extends Controller
         return redirect('/categorias');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $cat = Categoria::find($id);
@@ -69,13 +37,6 @@ class ControladorCategoria extends Controller
         return redirect('/categorias');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $cat = Categoria::find($id);
@@ -86,12 +47,6 @@ class ControladorCategoria extends Controller
         return redirect('/categorias');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $cat = Categoria::find($id);
@@ -104,8 +59,7 @@ class ControladorCategoria extends Controller
     public function indexJson()
     {
         $cats = Categoria::all();
-        return json_encode($cats);
-        //return response()->json($cats);
+        return json_encode($cats);       
     }
 }
 
