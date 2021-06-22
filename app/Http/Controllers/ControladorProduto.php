f9329abfa03ddde1a2b1553bd88ad807dfdb5b0c
Roberto <betobr12@users.noreply.github.com>
2021-06-22 13:39:03 -0300

    Update ControladorProduto.php

--- app/Http/Controllers/ControladorProduto.php
+++ app/Http/Controllers/ControladorProduto.php
@@ -8,11 +8,7 @@ use Illuminate\Http\Request;
 
 class ControladorProduto extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function indexView()
     {
         $produtos = Produto::all(); 
@@ -25,11 +21,6 @@ class ControladorProduto extends Controller
         return json_encode($produtos);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
 
@@ -38,12 +29,6 @@ class ControladorProduto extends Controller
         return view('novoproduto',compact('categorias'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $prod = new Produto();
@@ -55,13 +40,6 @@ class ControladorProduto extends Controller
         return json_encode($prod);
     }
 
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $prod = Produto::find($id);
@@ -71,12 +49,6 @@ class ControladorProduto extends Controller
         return response('Produto não encontrado', 404);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)        
     {
         $categorias = Categoria::all();
@@ -87,13 +59,6 @@ class ControladorProduto extends Controller
         return redirect('/produtos');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $prod = Produto::find($id);
@@ -108,12 +73,6 @@ class ControladorProduto extends Controller
         return response('Produto não encontrado', 404);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $prod = Produto::find($id);
